0e392710158c41071e378f97c6eda7d675b32cbf
Agustin Zucca <zucca.agustin98@gmail.com>
2022-06-17 19:05:23 -0500

    seeding and some fixes

--- app/seeds/images.py
+++ app/seeds/images.py
@@ -13,10 +13,50 @@ def seed_images():
         spot_id=3,
         url='https://photos.zillowstatic.com/fp/784f984ba106ec1f8146c0d026a47c24-uncropped_scaled_within_1536_1152.webp'
     )
+    img4 = Image(
+        spot_id=4,
+        url='https://luxbnb.s3.us-east-2.amazonaws.com/hamptons2.png'
+    )
+    img5 = Image(
+        spot_id=5,
+        url='https://luxbnb.s3.us-east-2.amazonaws.com/pic2.png'
+    )
+    img6 = Image(
+        spot_id=6,
+        url='https://luxbnb.s3.us-east-2.amazonaws.com/pic3.png'
+    )
+    img7 = Image(
+        spot_id=7,
+        url='https://luxbnb.s3.us-east-2.amazonaws.com/manhattan1.png'
+    )
+    img8 = Image(
+        spot_id=8,
+        url='https://luxbnb.s3.us-east-2.amazonaws.com/manhattan2.png'
+    )
+    img9 = Image(
+        spot_id=9,
+        url='https://luxbnb.s3.us-east-2.amazonaws.com/chicago1.png'
+    )
+    img10 = Image(
+        spot_id=10,
+        url='https://luxbnb.s3.us-east-2.amazonaws.com/dallas1.png'
+    )
+    img11 = Image(
+        spot_id=11,
+        url='https://luxbnb.s3.us-east-2.amazonaws.com/boston1.png'
+    )
 
     db.session.add(img1)
     db.session.add(img2)
     db.session.add(img3)
+    db.session.add(img4)
+    db.session.add(img5)
+    db.session.add(img6)
+    db.session.add(img7)
+    db.session.add(img8)
+    db.session.add(img9)
+    db.session.add(img10)
+    db.session.add(img11)
 
     db.session.commit()
 
--- app/seeds/spots.py
+++ app/seeds/spots.py
@@ -12,7 +12,7 @@ def seed_spots():
         description='Relax at this beautiful hosue on the hills of Los Angeles.',
         beds='4',
         baths='5',
-        price='500')
+        price='5000')
     spot2 = Spot(
         user_id=2,
         address='1469 Sunshine Ln',
@@ -33,11 +33,99 @@ def seed_spots():
         beds='6',
         baths='7',
         price='5000')
+    spot4 = Spot(
+        user_id=3,
+        address='683 Ocean Rd',
+        city='Southampton',
+        state='New York',
+        name='Hamptons Compound',
+        description="Exquisite grace and grandeur hallmark this magnificent masterpiece - beautifully preserving its authentic original details - offering 18,000+ square feet on three floors, including 8 principal bedrooms, 8 full and three half baths, and separate staff quarters. Basking in 10 +/- acres of park-like privacy on exclusive Ox Pasture Road, Linden sits in the heart of the Estate Section.",
+        beds='8',
+        baths='9',
+        price='25000')
+    spot5 = Spot(
+        user_id=2,
+        address='432 Newark Rd',
+        city='Southampton',
+        state='New Jersey',
+        name='Calm on the River',
+        description="Beautiful established home by the river. Come enjoy the breathtaking nature by staying at our place.",
+        beds='8',
+        baths='9',
+        price='4500')
+    spot6 = Spot(
+        user_id=2,
+        address='900 Pebble Ln',
+        city='Southampton',
+        state='New York',
+        name='New Hamptons Mansion',
+        description="Located between the oceanfront Maidstone Club and Main Street shopping, this stunning new traditional style home built custom by Ben Krupinksi Builder was just finished in 2022 is perfectly sited on a south-facing acre in East Hampton Village South.",
+        beds='7',
+        baths='7',
+        price='10000')
+    spot7 = Spot(
+        user_id=2,
+        address='45 St',
+        city='New York',
+        state='New York',
+        name='Manhattan Penthouse',
+        description="Set atop a landmark condominium in Soho rests this exquisite Corner Penthouse Duplex Loft boasting 2,421 sq ft of impeccably renovated interiors and an enormous rooftop terrace.",
+        beds='2',
+        baths='3',
+        price='10000')
+    spot8 = Spot(
+        user_id=2,
+        address='6th St',
+        city='New York',
+        state='New York',
+        name='King Penthouse',
+        description="Set atop a landmark condominium in Soho rests this exquisite Corner Penthouse Duplex Loft boasting 2,421 sq ft of impeccably renovated interiors and an enormous rooftop terrace.",
+        beds='3',
+        baths='4',
+        price='12000')
+    spot9 = Spot(
+        user_id=3,
+        address='1987 Stoney Rd',
+        city='Chicago',
+        state='Illinois',
+        name='Modern Chicago Vibes',
+        description="Meticulously designed for family living and sustainability, this contemporary 10,400 square foot, 5 bedroom home on a 45 x 145 foot lot is among the newest additions to Chicago's historic Astor Street District.",
+        beds='5',
+        baths='5',
+        price='6000')
+    spot10 = Spot(
+        user_id=3,
+        address='10333 Woodford Dr',
+        city='Dallas',
+        state='Texas',
+        name='Mansion in Dallas',
+        description="Certain expectations come with living in certain zip codes at certain price points. Effortlessly exceeding them all is this 12,379 sq ft Simmie Cooper custom build 2010. The exclusive estate neighborhood combined with the home's intuitive layout, luxe amenities, extensive custom upgrades and handsome contemporary finishes, create a sense of having arrived.",
+        beds='3',
+        baths='4',
+        price='9000')
+    spot11 = Spot(
+        user_id=3,
+        address='55 Commercial Wharf #12',
+        city='Boston',
+        state='Massachusetts',
+        name='Boston Lux Aparment',
+        description="Exceptional designer waterfront home. Two combined floor-through units create a rare and coveted floor plan that lives like a single family with the ease of one floor living.  Customized 3 bed 3 bath expands across two wings with 2565 sq ft. Live, entertain and dine in one wing and retreat to a separate primary suite sanctuary in the other.",
+        beds='3',
+        baths='3',
+        price='7000')
     
 
     db.session.add(spot1)
     db.session.add(spot2)
     db.session.add(spot3)
+    db.session.add(spot4)
+    db.session.add(spot5)
+    db.session.add(spot6)
+    db.session.add(spot7)
+    db.session.add(spot8)
+    db.session.add(spot9)
+    db.session.add(spot10)
+    db.session.add(spot11)
 
     db.session.commit()
 
